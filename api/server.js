5b3910729e26a5961408ac9e8d2d8a6f6fd10894
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-02-18 18:44:24 -0500

    trying to get deployment

--- api/server.js
+++ api/server.js
@@ -7,14 +7,14 @@ const server = express();
 server.use(logger())
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json())
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 server.use(userRouter)
 server.use(postRouter)
 
 // global middlewares and routes need to be connected here
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
 
 server.use((err,req,res,next)=>{
   res.status(500).json({message:"Danger Will Robinson!!! Something went wrong!! I can feel it!"})
